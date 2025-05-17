169ccf7d78da4748e6c9b5f91a5f1bfd8899bfa5
Mustafa  Adam <177422446+mustafaChemEng@users.noreply.github.com>
2025-05-18 00:24:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ from modules import electrolyzer  # only import modules that are ready
 #st.sidebar.image("assets/images/logo.png", use_column_width=True)
 #st.sidebar.markdown("### 👋 Welcome, Engineer!")
 #st.sidebar.markdown("Explore sustainable hydrogen design tools 🌱")
-import streamlit as st
-import sys
-import os
+
 
 st.write("Python path:", sys.path)
 st.write("Modules folder exists:", os.path.isdir("modules"))
